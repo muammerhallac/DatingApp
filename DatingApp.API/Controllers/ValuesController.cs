3f3f5a0f74a1cb5e779c1d23feeb7acf0e77a86b
Muammer HALLAC <muammer.hallac@hepsiburada.com>
2018-12-16 14:52:47 +0300

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -23,6 +23,7 @@ namespace DatingApp.API.Controllers
 
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> GetValues()
         {
             var values = await dataContext.Values.ToListAsync();
